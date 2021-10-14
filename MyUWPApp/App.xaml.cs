a12d6de0564aa3694c4231ed724d6b69791bb17c
Matthias Wang <matthiaswang60@gmail.com>
2021-10-14 16:06:11 +0800

    revert commit

--- MyUWPApp/App.xaml.cs
+++ MyUWPApp/App.xaml.cs
@@ -1,8 +1,4 @@
-﻿using MUXC = Microsoft.UI.Xaml.Controls;
-using System.Globalization;
-using Microsoft.Toolkit.Win32.UI.XamlHost;
-
-namespace MyUWPApp
+﻿namespace MyUWPApp
 {
     public sealed partial class App : Microsoft.Toolkit.Win32.UI.XamlHost.XamlApplication
     {
