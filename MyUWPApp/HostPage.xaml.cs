cb0bc0f579d0265e7aaea8b63fe56f1827b7b626
Matthias Wang <matthiaswang60@gmail.com>
2021-10-14 22:19:12 +0800

    WinUI 2.7 Test Commit (will be reverted)

--- MyUWPApp/HostPage.xaml.cs
+++ MyUWPApp/HostPage.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using MUXC = Microsoft.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -36,7 +37,7 @@ namespace MyUWPApp
             NavView.SelectedItem = Page1;
         }
 
-        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        private void NavView_SelectionChanged(MUXC.NavigationView sender, MUXC.NavigationViewSelectionChangedEventArgs args)
         {
             //this part controls which page to navigate to, upon user selection of a new NavViewItem
             if (NavView.SelectedItem == Page1)
